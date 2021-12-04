59ccbceb1548bb5ab351f8dd5bb055be019dab58
zaferaslan <zafer-aslan99@hotmail.com>
2021-12-04 14:34:06 +0300

    Certification Task 2/4

---
+++ MvcDpuSozluk/BusinessLayer/Abstract/IHeadingService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IHeadingService
+    {
+        List<Heading> GetList();
+        List<Heading> GetListByWriter(int id);
+        List<Heading> GetListByCategory(int id);
+        void HeadingAddBL(Heading heading);
+        Heading GetByID(int id);
+        Heading GetByCatID(int id);
+    }
+}
